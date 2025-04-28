30c076df4d4ab8f788d7d01dea29d49f8a562c89
humhaiprashant <humhaiprashant@gmail.com>
2025-04-28 19:43:01 +0530

    updated commit

--- app/src/main/java/com/example/customcalendar/utils/DateHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.example.customcalendar.utils
-
-data class DateHolder(
-    val day: Int,
-    val month: Int,
-    val year: Int
-)
--- app/src/main/java/com/example/customcalendar/utils/DateUtils.kt
+++
@@ -1,4 +0,0 @@
-package com.example.customcalendar.utils
-
-val Int.isLeapYear: Boolean
-    get() = (this % 4 == 0 && this % 100 != 0) || (this % 400 == 0)
